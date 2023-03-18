286cc5fd624eabf11e84a580503dff235dfc9e27
Savannah Vel <savannah.vel@vivint.com>
2023-03-17 21:27:17 -0700

    manifest loading successfully

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -23,22 +20,27 @@ module.exports = () => {
         title: 'Webpack Plugin',
       }),
       new WebpackPwaManifest({
-        "name": "Scribble",
-        "short_name": "Scribble",
-        "start_url": ".",
-        "display": "standalone",
-        "background_color": "#fff",
-        "description": "A simple text editor to scribble your thoughts on",
-        "icons": [
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        start_url: "./",
+        publicPath: './',
+        display: "standalone",
+        background_color: "#fff",
+        description: "Takes notes with Javascript syntax highlighting!",
+        icons: [
           {
-            "src": "./src/images/logo.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any maskable"
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
           }
         ],
-        "orientation": "portrait",
-        "theme_color": "#7eb4e2"
+        orientation: "portrait",
+        theme_color: "#225ca3",
+        background_color: "#225ca3"
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
     ],
 
